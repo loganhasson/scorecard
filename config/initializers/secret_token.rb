d9e30b8648658ed7edc0e07cc4aeaae67cf38104
Logan Hasson <logan.hasson@gmail.com>
2013-11-06 18:51:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scorecard::Application.config.secret_key_base = 'f72e96655bd54df4a37433d6b1bac87a70fa33681a74cc45f090bf1701fe63c931598ad9235e44b031859e54533e0ab64199abe9c1933302b1b07f0a03f4fec9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scorecard::Application.config.session_store :cookie_store, key: '_scorecard_session'
